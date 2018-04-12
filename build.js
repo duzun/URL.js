6a97f4a941bbc125df0e5b16e91d31f35a75d644
Dumitru Uzun <contact@duzun.me>
2018-04-12 17:07:17 +0300

    Use gccs.js to compile

--- build.js
+++ build.js
@@ -1,10 +1,12 @@
 /**
  *   Compile JS files using Closure-Compiler service
  */
-var http        = require('http');
-var fs          = require('fs');
-var path        = require('path');
-var querystring = require('querystring');
+
+/*jshint node: true*/
+
+var fs   = require('fs');
+var path = require('path');
+var gccs = require('gccs');
 
 
 var dir = __dirname;
@@ -15,66 +17,39 @@ var version = packo.version;
 var hashes = [path.basename(packo.main, '.js')];
 
 if ( !fs.existsSync(dist) ) {
-    fs.mkdir(dist)
+    fs.mkdir(dist);
 }
 
 hashes.forEach(function (name) {
     var filename = path.join(dir, name + '.js');
-    fs.createReadStream(filename).pipe(fs.createWriteStream(path.join(dist, name + '.' + version + '.js')));
-    compileFile(filename, function (err, data) {
+
+    var dest_filename         = path.join(dist, name + '.js');
+    var dest_min_filename     = path.join(dist, name + '.min.js');
+    var dest_ver_filename     = path.join(dist, name + '.' + version + '.js');
+    var dest_min_ver_filename = path.join(dist, name + '.' + version + '.min.js');
+
+    cp(filename, dest_filename);
+    cp(filename, dest_ver_filename, function () {
+        log_green(dest_ver_filename);
+    });
+
+    gccs.file(filename, dest_min_ver_filename, function (err) {
         if (err) {
             return console.error(err);
         }
-        if ( data ) {
-            var filename = path.join(dist, name + '.' + version + '.min.js');
-            fs.writeFile(filename, data.trim(), function (err) {
-                if (err) throw err;
-                console.log("\x1b[32m%s\x1b[0m", filename);
-            })
-        }
+        log_green(dest_min_ver_filename);
+        cp(dest_min_ver_filename, dest_min_filename);
     });
 });
 
-// Helpers
-function compileFile(filename, cb) {
-    fs.readFile(
-      filename
-      , { encoding: 'utf-8' }
-      , function (err, data) { err ? cb(err) : compile(data, cb) }
-    );
+function cp(src, dest, cb) {
+    var s = fs.createReadStream(src).pipe(fs.createWriteStream(dest));
+    if ( cb ) {
+        s.on('finish', cb);
+    }
+    return s;
 }
 
-function compile(script, cb) {
-    var options = {
-        output_info: 'compiled_code'
-      , output_format: 'text'
-      , compilation_level: 'SIMPLE_OPTIMIZATIONS'
-      , warning_level: 'QUIET'
-      , js_code: script
-    };
-
-    var data = querystring.stringify(options);
-
-    var req = http.request({
-      hostname: 'closure-compiler.appspot.com'
-      , port: 80
-      , path: '/compile'
-      , method: 'POST'
-      , headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': Buffer.byteLength(data)
-      }
-    }, function(res) {
-      // console.log('STATUS: ' + res.statusCode);
-      var body = [];
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) { body.push(chunk) });
-      res.on('end', function () { cb(null, body = body.join(''), res) });
-    });
-
-    req.on('error', cb);
-
-    req.write(data);
-    req.end();
+function log_green(txt) {
+    console.log("\x1b[32m%s\x1b[0m", txt);
 }
-
